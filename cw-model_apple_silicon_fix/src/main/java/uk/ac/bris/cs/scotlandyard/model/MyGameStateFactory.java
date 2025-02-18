81b202620b0dfe88d87665ddcf4345263b682282
Oliver <ph24475@bristol.ac.uk>
2025-02-18 17:20:15 +0000

    Added null placeholders for the 8 methods required in MyGameStateFactory

--- cw-model_apple_silicon_fix/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ cw-model_apple_silicon_fix/src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,8 +4,15 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+//import java.util.*;
+//import javax.annotation.Nonnull;
+//import uk.ac.bris.cs.scotlandyard.model.Move.*;
+//import uk.ac.bris.cs.scotlandyard.model.Piece.*;
+//import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
+import java.util.Optional;
 
 /**
  * cw-model
@@ -17,9 +24,17 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			GameSetup setup,
 			Player mrX,
 			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
-
+		return new MyGameState();
 	}
-
-}
+		// TODO
+		private final class MyGameState implements GameState {
+			@Override public GameSetup getSetup() {  return null; }
+			@Override public ImmutableSet<Piece> getPlayers() { return null; }
+			@Override public GameState advance(Move move) {  return null;  }
+			@Override public Optional<Integer> getDetectiveLocation(Piece.Detective detective) { return Optional.empty(); }
+			@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) { return Optional.empty(); }
+			@Override public ImmutableList<LogEntry> getMrXTravelLog() { return null; }
+			@Override public ImmutableSet<Piece> getWinner() { return null; }
+			@Override public ImmutableSet<Move> getAvailableMoves() { return null; }
+		}
+	}
